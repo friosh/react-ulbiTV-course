074278ff777d5870462c9eec61b9bfa2ba5f7b1c
Friosh <andrey.u90@mail.ru>
2023-02-26 19:13:03 +0300

    add router config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { classNames } from "shared/lib/classNames/classNames"
 import { useTheme } from "./providers/ThemeProvider"
 import { AboutPage } from "pages/AboutPage"
 import { MainPage } from "pages/MainPage"
+import { AppRouter } from "./providers/router"
 
 export const App = () => {
     const {theme, toggleTheme} = useTheme();
@@ -14,11 +15,6 @@ export const App = () => {
         <button onClick={toggleTheme}>Change Theme</button>
         <Link to="/">Main page</Link>
         <Link to="/about">About</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
-                </Routes>
-            </Suspense>
+        <AppRouter />
     </div>
 }
---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,13 @@
+import { Suspense } from "react"
+import { Routes, Route } from "react-router-dom"
+import { routeConfig } from "shared/config/routeConfig/routeConfig"
+
+export const AppRouter = () => {
+    return <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+            {Object.values(routeConfig).map(({ element, path }) => (
+                <Route key={path} path={path} element={element} />
+            ))}
+        </Routes>
+    </Suspense>
+}
